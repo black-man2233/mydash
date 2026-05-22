c7fcc2f836fcf67cf0dde98c842edf1b992d2872
archabuser <kevi3758@gmail.com>
2026-05-22 13:51:09 +0200

    feat(hub): wire up Program.cs with DI, auth, EF Core, gRPC, Serilog

--- src/MyDash.Hub/Program.cs
+++ src/MyDash.Hub/Program.cs
@@ -1,24 +1,120 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using MyDash.Application.Options;
+using MyDash.Application.Repositories;
+using MyDash.Application.Services;
+using MyDash.Application.UseCases;
 using MyDash.Hub.Components;
+using MyDash.Infrastructure.Data;
+using MyDash.Infrastructure.GrpcServices;
+using MyDash.Infrastructure.Repositories;
+using MyDash.Infrastructure.Services;
+using MyDash.Infrastructure.Sms;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/hub-.log", rollingInterval: RollingInterval.Day)
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .ReadFrom.Configuration(ctx.Configuration)
+        .WriteTo.Console()
+        .WriteTo.File("logs/hub-.log", rollingInterval: RollingInterval.Day));
+
+    builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+    builder.Services.AddGrpc();
+
+    var connStr = builder.Configuration["Database:ConnectionString"] ?? "Data Source=mydash.db";
+    builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlite(connStr));
+
+    builder.Services.Configure<SmsOptions>(builder.Configuration.GetSection("Sms"));
+    builder.Services.Configure<SecurityOptions>(builder.Configuration.GetSection("Security"));
+    builder.Services.Configure<HubOptions>(builder.Configuration.GetSection("Hub"));
+
+    builder.Services.AddScoped<IServerRepository, EfServerRepository>();
+    builder.Services.AddScoped<IServiceRepository, EfServiceRepository>();
+    builder.Services.AddScoped<IEnrollmentTokenRepository, EfEnrollmentTokenRepository>();
+    builder.Services.AddScoped<IAuditRepository, EfAuditRepository>();
+    builder.Services.AddScoped<IPinChallengeRepository, EfPinChallengeRepository>();
+    builder.Services.AddScoped<IUserPreferencesRepository, EfUserPreferencesRepository>();
+
+    builder.Services.AddScoped<IAuditWriter, AuditWriter>();
+    builder.Services.AddScoped<RequestPinHandler>();
+    builder.Services.AddScoped<VerifyPinHandler>();
+
+    var smsProvider = builder.Configuration["Sms:Provider"] ?? "TextBelt";
+    builder.Services.AddHttpClient<TextBeltSmsSender>();
+    switch (smsProvider)
+    {
+        case "Twilio":
+            builder.Services.AddScoped<ISmsSender, TwilioSmsSender>();
+            break;
+        case "Vonage":
+            builder.Services.AddHttpClient<VonageSmsSender>();
+            builder.Services.AddScoped<ISmsSender, VonageSmsSender>();
+            break;
+        case "ClickSend":
+            builder.Services.AddHttpClient<ClickSendSmsSender>();
+            builder.Services.AddScoped<ISmsSender, ClickSendSmsSender>();
+            break;
+        case "SMSAPI":
+            builder.Services.AddHttpClient<SmsApiSmsSender>();
+            builder.Services.AddScoped<ISmsSender, SmsApiSmsSender>();
+            break;
+        default:
+            builder.Services.AddScoped<ISmsSender, TextBeltSmsSender>();
+            break;
+    }
+
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(o =>
+        {
+            o.LoginPath = "/login";
+            o.AccessDeniedPath = "/login";
+            o.SlidingExpiration = true;
+            var hours = builder.Configuration.GetValue<int>("Security:SessionCookieHours", 12);
+            o.ExpireTimeSpan = TimeSpan.FromHours(hours);
+        });
+    builder.Services.AddAuthorization();
+    builder.Services.AddCascadingAuthenticationState();
+    builder.Services.AddHttpContextAccessor();
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.EnsureCreated();
+    }
+
+    if (!app.Environment.IsDevelopment())
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseAntiforgery();
+
+    app.MapGrpcService<AuthGrpcService>();
+    app.MapGrpcService<FleetGrpcService>();
+    app.MapGrpcService<AgentGrpcService>();
+
+    app.MapStaticAssets();
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+    app.Run();
 }
-
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    Log.Fatal(ex, "Hub terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
